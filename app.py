75dd0bec799426bf981b56097a45a82b681eed7a
faran1000 <farzanehmortezapour53@gmail.com>
2026-09-25 11:03:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 import sqlite3
 from datetime import datetime
 import jdatetime
-
+import os
 app = Flask(__name__)
 app.secret_key = "GAPGPTMASKTOKEN69rbztv0ezX0X"
 
@@ -286,5 +286,6 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
